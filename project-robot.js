4ab3f44ce9a71f0a7c22aff8eb6ce4d7ffdc3782
pdriding <peterdriding@gmail.com>
2024-11-19 11:17:39 +0400

    Added compare robot function

--- project-robot.js
+++ project-robot.js
@@ -25,7 +25,7 @@ function buildGraph(edges) {
       graph[from] = [to];
     }
   }
-  console.log(edges.map((r) => r.split("-")));
+  // console.log(edges.map((r) => r.split("-")));
   for (let [from, to] of edges.map((r) => r.split("-"))) {
     addEdge(from, to);
     addEdge(to, from);
@@ -35,7 +35,7 @@ function buildGraph(edges) {
 
 const roadGraph = buildGraph(roads);
 
-console.log(roadGraph);
+// console.log(roadGraph);
 
 class VillageState {
   constructor(place, parcels) {
@@ -58,18 +58,16 @@ class VillageState {
   }
 }
 
-function runRobot(state, robot, memory) {
-  for (let turn = 0; ; turn++) {
-    if (state.parcels.length == 0) {
-      console.log(`Done in ${turn} turns`);
-      break;
-    }
-    let action = robot(state, memory);
-    state = state.move(action.direction);
-    memory = action.memory;
-    console.log(`Moved to ${action.direction}`);
-  }
-}
+// function runRobot(state, robot, memory) {
+//   for (let turn = 0; ; turn++) {
+//     if (state.parcels.length == 0) {
+//       return turn;
+//     }
+//     let action = robot(state, memory);
+//     state = state.move(action.direction);
+//     memory = action.memory;
+//   }
+// }
 
 function randomPick(array) {
   let choice = Math.floor(Math.random() * array.length);
@@ -93,8 +91,94 @@ VillageState.random = function (parcelCount = 5) {
   return new VillageState("Post Office", parcels);
 };
 
-runRobot(VillageState.random(), randomRobot);
-// → Moved to Marketplace
-// → Moved to Town Hall
-// → …
-// → Done in 63 turns
+// runRobot(VillageState.random(), randomRobot);
+
+const mailRoute = [
+  "Alice's House",
+  "Cabin",
+  "Alice's House",
+  "Bob's House",
+  "Town Hall",
+  "Daria's House",
+  "Ernie's House",
+  "Grete's House",
+  "Shop",
+  "Grete's House",
+  "Farm",
+  "Marketplace",
+  "Post Office",
+];
+
+function routeRobot(state, memory) {
+  if (memory.length == 0) {
+    memory = mailRoute; // Reset to the full route if memory is empty
+  }
+  return { direction: memory[0], memory: memory.slice(1) }; // Move to the next place
+}
+
+//-----------------------------------------------------------------------
+
+function goalOrientedRobot({ place, parcels }, route) {
+  if (route.length == 0) {
+    // No planned route
+    let parcel = parcels[0]; // Focus on the first parcel
+    if (parcel.place != place) {
+      // Parcel is not yet picked up
+      route = findRoute(roadGraph, place, parcel.place);
+    } else {
+      // Parcel is picked up, deliver it
+      route = findRoute(roadGraph, place, parcel.address);
+    }
+  }
+  return { direction: route[0], memory: route.slice(1) }; // Move and update memory
+}
+
+function findRoute(graph, from, to) {
+  let work = [{ at: from, route: [] }];
+  for (let i = 0; i < work.length; i++) {
+    let { at, route } = work[i];
+    for (let place of graph[at]) {
+      if (place == to) return route.concat(place);
+      if (!work.some((w) => w.at == place)) {
+        work.push({ at: place, route: route.concat(place) });
+      }
+    }
+  }
+}
+
+//---------------------------------------------------------------------------------
+
+function runRobot(state, robot, memory) {
+  for (let turns = 0; ; turns++) {
+    console.log(1111111111, turns);
+    console.log(555, state.parcels);
+    if (state.parcels.length === 0) {
+      console.log(1111, turns);
+      return turns;
+    }
+    const hmm = robot(state, memory);
+    memory = hmm.memory;
+    state = state.move(hmm.direction);
+
+    console.log(8888, state);
+  }
+
+  // console.log(robot);
+}
+
+function compareRobots(robot1, memory1, robot2, memory2) {
+  let z = 0;
+  let x = 0;
+  for (let i = 0; i < 100; i++) {
+    const state = VillageState.random();
+    const a = runRobot(state, robot1, memory1);
+    const b = runRobot(state, robot2, memory2);
+    z += a;
+    x += b;
+  }
+  x = Math.round(x / 100);
+  z = Math.round(z / 100);
+  console.log(`ROBOT 1: ${x} turns, ROBOT 2: ${z} turns`);
+}
+
+compareRobots(routeRobot, [], goalOrientedRobot, []);
